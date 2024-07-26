2866f7d436bebde50cc5825271598f0e28e8ae61
Mingle <bevolutionstudios@gmail.com>
2024-07-26 12:27:05 -0700

    Updated the script

--- task_2.sql
+++ task_2.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 );
 
 -- Create the 'books' table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 -- Create the 'customers' table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 -- Create the 'orders' table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- Create the 'order_details' table
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
